29497dc344edf56ceae7482a9309c09c3df43fc5
manibrar <manvirbrar1988@gmail.com>
2018-06-15 09:17:45 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var player = 0;
+  var player2 = 0;
+  var grid = document.getElementsByTagName('td');
+  // grid[4].innerHTML = "hello";
+console.log(grid);
+
+
+  //
+  //     grid.addEventListener('click', function(event) {
+  //         if (player === 1) {
+  //           alert("Your turn player 2")
+  //           grid.innerHTML = "o";
+  //           player = 0;
+  //       };
+  //     });
+  });
