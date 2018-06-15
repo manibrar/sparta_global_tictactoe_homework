2aca0bdb53ec6e954b84ff970894bd9bdb2f4b2d
manibrar <manvirbrar1988@gmail.com>
2018-06-15 11:43:53 +0100

    working

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var player = false;
+var grid = document.querySelectorAll('td');
+var turntext = document.que
+for (var i = 0; i < grid.length; i++) {
+  grid[i].addEventListener('click', function(event) {
+    if 
+  });
+}
